af4899f37bfcd43675d40689c7774624837fc543
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-26 02:28:56 +0500

    for loops updated

--- 07_for_loop.js
+++ 07_for_loop.js
@@ -5,3 +5,13 @@ for (let i = 1; i <= 10; i++) {
 for (let i = 1; i <= 10; i += 3) {
 	console.log('Hello:', i); // Hello: 1,4,7,10
 }
+
+// let's generate perfect squares:
+for (let i = 1; i <= 20; i++) {
+	console.log(`${i} x ${i} = ${i * i}`); // 1, 4, 9, 16...
+}
+
+// 50, 40, 30, 20, 10, 0
+for (let i = 50; i >= 0; i -= 10) {
+	console.log(i);
+}
