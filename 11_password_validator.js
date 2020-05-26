f98b15fa0ccbe6c61bea61698d3628f9a4e1eebd
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-26 09:07:57 +0500

    password validator - colt's solution

--- 11_password_validator.js
+++ 11_password_validator.js
@@ -11,11 +11,50 @@
 // isValidPassword('dogLuvr123!', 'dogLuvr') //false
 // isValidPassword('hello1', 'dogLuvr') //false
 
-// my solution:
+// colt's solution:
 function isValidPassword(password, username) {
-	if (password.length >= 8 && password.indexOf(' ') === -1 && password.indexOf(username) === -1) {
-		return true;
-	} else {
+	if (password.length < 8) {
 		return false;
 	}
+	if (password.indexOf(' ') !== -1) {
+		return false;
+	}
+	if (password.indexOf(username) !== -1) {
+		return false;
+	}
+	return true;
+}
+
+// other way:
+function isValidPassword(password, username) {
+	if (password.length < 8 || password.indexOf(' ') !== -1 || password.indexOf(username) !== -1) {
+		return false;
+	}
+	return true;
+}
+
+// other way 2:
+function isValidPassword(password, username) {
+	const tooShort = password.length < 8;
+	const hasSpace = password.indexOf(' ') !== -1;
+	const tooSimilar = password.indexOf(username) !== -1;
+	if (tooShort || hasSpace || tooSimilar) return false;
+	return true;
+}
+
+// other way 3:
+function isValidPassword(password, username) {
+	const tooShort = password.length < 8;
+	const hasSpace = password.indexOf(' ') !== -1;
+	const tooSimilar = password.indexOf(username) !== -1;
+	if (!tooShort && !hasSpace && !tooSimilar) return true;
+	return false;
+}
+
+// other way 3.1:
+function isValidPassword(password, username) {
+	const tooShort = password.length < 8;
+	const hasSpace = password.indexOf(' ') !== -1;
+	const tooSimilar = password.indexOf(username) !== -1;
+	return !tooShort && !hasSpace && !tooSimilar; // no need in returning false - it will be returned if those conditions will not met
 }
