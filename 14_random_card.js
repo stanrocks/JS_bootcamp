1a1533c96d14ae549e541f34ceeb2e18d679f1c9
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-26 10:27:57 +0500

    random card generator - colt's solution

--- 14_random_card.js
+++ 14_random_card.js
@@ -9,11 +9,27 @@
 //----clubs,spades, hearts, diamonds
 //Return both in an object
 
-// my solution:
+// Colt's solution:
 function getCard() {
-	const values = [ 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K' ];
+	const values = [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' ];
+	const valIdx = Math.floor(Math.random() * values.length);
+	const value = values[valIdx];
+
 	const suits = [ 'clubs', 'spades', 'hearts', 'diamonds' ];
-	let genValue = Math.floor(Math.random() * 13);
-	let genSuit = Math.floor(Math.random() * 4);
-	return { value: values[genValue], suit: suits[genSuit] };
+	const suitIdx = Math.floor(Math.random() * suits.length);
+	const suit = suits[suitIdx];
+	return { value: value, suit: suit };
+}
+
+// Colt's solution 2:
+function pick(arr) {
+	//return random element from arr
+	const idx = Math.floor(Math.random() * arr.length);
+	return arr[idx];
+}
+
+function getCard() {
+	const values = [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' ];
+	const suits = [ 'clubs', 'spades', 'hearts', 'diamonds' ];
+	return { value: pick(values), suit: pick(suits) };
 }
