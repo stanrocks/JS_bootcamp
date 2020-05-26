71947828752ec833d2fae7f43e3e5c86794df4aa
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-27 03:18:50 +0500

    high order functions update

--- 17_high_order_functions.js
+++ 17_high_order_functions.js
@@ -1,3 +1,5 @@
+// =========== FUNCTIONS ARE OBJECTS ===========
+
 function add(x, y) {
 	return x + y;
 }
@@ -28,3 +30,84 @@ const thing = {
 	doSomething: multiply
 };
 thing.doSomething(4, 5); //20
+// this is a method
+
+// =========== HIGHER ORDER FUNCTIONS ===========
+
+// This function accepts another function as an argument
+function callThreeTimes(f) {
+	//And calls it 3 times:
+	f();
+	f();
+	f();
+}
+
+function cry() {
+	console.log("BOO HOO I'M SO SAD!");
+}
+
+function rage() {
+	console.log('I HATE EVERYTHING!');
+}
+
+function repeatNTimes(action, num) {
+	// call action (a function) num number of times
+	for (let i = 0; i < num; i++) {
+		action();
+	}
+}
+
+repeatNTimes(rage, 13);
+
+// Accepts 2 functions as arguments
+// Randomly selects 1 to execute
+function pickOne(f1, f2) {
+	let rand = Math.random();
+	if (rand < 0.5) {
+		f1();
+	} else {
+		f2();
+	}
+}
+
+// RETURN A FUNCTION FROM WITHIN A FUNCTION
+
+// This function returns a function!
+function multiplyBy(num) {
+	return function(x) {
+		return x * num;
+	};
+}
+
+//triple holds a function:
+const triple = multiplyBy(3);
+//we can call it:
+triple(4); //12
+triple(10); //30
+
+const double = multiplyBy(2);
+double(3); //6
+double(9); //18
+
+const halve = multiplyBy(0.5);
+halve(5); //2.5
+halve(100); //50
+
+// This function also acts as a "function factory"
+function makeBetweenFunc(x, y) {
+	return function(num) {
+		return num >= x && num <= y;
+	};
+}
+// This function checks if a value is between 0 and 18
+const isChild = makeBetweenFunc(0, 18);
+isChild(10); //true
+isChild(56); //false
+
+const isInNineties = makeBetweenFunc(1990, 1999);
+isInNineties(1994); //true
+isInNineties(1987); //false
+
+const isNiceWeather = makeBetweenFunc(60, 79);
+isNiceWeather(68); //true
+isNiceWeather(98); //false
