0db1b4900aea15c66fbb554a2e5f8c31c80d876a
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-27 01:47:55 +0500

    scopes updated

--- 15_scopes.js
+++ 15_scopes.js
@@ -28,3 +28,62 @@ function birdWatch() {
 }
 birdWatch();
 console.log(bird); //"mandarin duck"
+
+// =========== BLOCK SCOPE ===========
+
+// let & const are BLOCK SCOPED
+if (true) {
+	const animal = 'eel';
+	console.log(animal); //'eel'
+}
+console.log(animal); //NOT DEFINED!
+
+// VAR IS NOT BLOCK SCOPED
+if (true) {
+	var animal = 'eel';
+	console.log(animal); //'eel'
+}
+console.log(animal); //'eel'
+
+// let animals = ['grizzly bear', 'panda bear', 'spectacled bear'];
+// var i = 10;
+// for (var i = 0; i < animals.length; i++) {
+//   console.log(i, animals[i])
+// }
+// console.log(i)
+
+// let animals = ['grizzly bear', 'panda bear', 'spectacled bear'];
+// let i = 10;
+// for (let i = 0; i < animals.length; i++) {
+//   console.log(i, animals[i])
+// }
+// console.log(i)
+
+function doubleArr(arr) {
+	const result = []; //scoped to the doubleArr function
+	for (let num of arr) {
+		let double = num * 2; //scoped to this loop
+		result.push(double);
+	}
+	return result;
+}
+
+// =========== LEXICAL SCOPE ===========
+
+function outer() {
+	let movie = 'Amadeus';
+
+	function inner() {
+		// let movie = "The Shining";
+
+		function extraInner() {
+			//movie is not defined in this function
+			//but it has access to parent function's variables
+			console.log(movie.toUpperCase());
+		}
+		extraInner();
+	}
+	inner();
+}
+
+outer(); //'AMADEUS'
