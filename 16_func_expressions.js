07dc235e77041767a45e20864970984aad78215e
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-27 02:45:51 +0500

    function expression

--- 16_func_expressions.js
+++ 16_func_expressions.js
@@ -2,13 +2,17 @@
 function add(x, y) {
 	return x + y;
 }
+add(1, 2); // 3
 
 // Function Expression (Anonymous)
 const sum = function(x, y) {
 	return x + y;
 };
+sum(1, 2); // 3
 
 // Function Expression (Named)
 const product = function multiply(x, y) {
 	return x * y;
 };
+product(2, 3); // 6
+multiply(2, 3); // DOES NOT WORK
