19b953c0671b4efc2dc2060d9565ac7588b47bee
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-26 05:04:42 +0500

    for...of with objects

--- 09_for_of_loop.js
+++ 09_for_of_loop.js
@@ -19,7 +19,7 @@ for (let char of 'cockadoodledoo') {
 	console.log(char.toUpperCase());
 }
 
-// FOR...OF vs REGULAR FOR
+// ================ FOR...OF vs REGULAR FOR ================
 
 const magicSquare = [ [ 2, 7, 6 ], [ 9, 5, 1 ], [ 4, 3, 8 ] ];
 
@@ -51,3 +51,37 @@ for (let i = 0; i < words1.length; i++) {
 	//Access index i of both arrays
 	console.log(`${words1[i]}${words2[i]}`);
 }
+
+// ================ FOR...OF WITH OBJECTS ================
+
+const movieReviews = {
+	Arrival: 9.5,
+	Alien: 9,
+	Amelie: 8,
+	'In Bruges': 9,
+	Amadeus: 10,
+	'Kill Bill': 8,
+	'Little Miss Sunshine': 8.5,
+	Coraline: 7.5
+};
+
+// THIS DOES NOT WORK!
+// OBJECTS ARE NOT ITERABLE (can't use a for...of loop)
+// for (let x of movieReviews) {
+// 	console.log(x);
+// }
+
+// We CAN iterate over the keys of an object
+for (let movie of Object.keys(movieReviews)) {
+	console.log(`You rated ${movie} - ${movieReviews[movie]}`);
+}
+
+// We can also iterate over the values
+// To calculate the average movie rating:
+const ratings = Object.values(movieReviews);
+let total = 0;
+for (let r of ratings) {
+	total += r;
+}
+let avg = total / ratings.length;
+console.log('Average Rating: ', avg);
