c0bc0a220733578df4df53a243688fd7f79df5f9
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-28 02:45:52 +0500

    for...in loop update

--- 09_2_for_in_loop.js
+++ 09_2_for_in_loop.js
@@ -23,3 +23,24 @@ for (let index in str) {
 
 // Index of T: 0
 // Index of u: 1
+
+// ================================================
+
+const jeopardyWinnings = {
+	regularPlay: 2522700,
+	watsonChallenge: 300000,
+	tournamentOfChampions: 500000,
+	battleOfTheDecades: 100000
+};
+
+for (let prop in jeopardyWinnings) {
+	console.log(prop); // regularPlay... prints keys each on new line
+	console.log(jeopardyWinnings[prop]); // 2522700 ...
+}
+
+let total = 0;
+for (let prop in jeopardyWinnings) {
+	total += jeopardyWinnings[prop]; // 3422700
+}
+
+console.log(`Ken Jennings Total Earnings: ${total}`);
