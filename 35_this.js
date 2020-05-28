942420a2fc1c52aff4d28d9648208d5f4c32fab1
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-28 06:23:19 +0500

    this keyword

---
+++ 35_this.js
@@ -0,0 +1,29 @@
+// ========= this in functions =========
+
+function sayHi() {
+	console.log('HI');
+	//this refers to the window (global scope object in the browser)
+	console.log(this);
+}
+
+const greet = function() {
+	//this refers to the window (global scope object in the browser)
+	console.log(this);
+};
+
+// ========= this in methods =========
+
+const person = {
+	first: 'Cherilyn',
+	last: 'Sarkisian',
+	nickName: 'Cher',
+	fullName() {
+		//In a method, this refers to the object the method "lives" in:
+		const { first, last, nickName } = this;
+		return `${first} ${last} AKA ${nickName}`;
+	},
+	printBio() {
+		const fullName = this.fullName();
+		console.log(`${fullName} is a person!`);
+	}
+};
