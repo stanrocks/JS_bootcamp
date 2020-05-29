20964bdc3e21eb98ef876e60753c5105d9953f1d
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-29 19:42:05 +0500

    DOM - coin game

--- DOM/15_coin_game.js
+++ DOM/15_coin_game.js
@@ -9,3 +9,44 @@ function isTouching(a, b) {
 		aRect.left > bRect.left + bRect.width
 	);
 }
+
+const avatar = document.querySelector('#player');
+const coin = document.querySelector('#coin');
+
+window.addEventListener('keyup', function(e) {
+	if (e.key === 'ArrowDown' || e.key === 'Down') {
+		moveVertical(avatar, 50);
+	} else if (e.key === 'ArrowUp' || e.key === 'Up') {
+		moveVertical(avatar, -50);
+	} else if (e.key === 'ArrowLeft' || e.key === 'Left') {
+		moveHorizontal(avatar, -50);
+		avatar.style.transform = 'scale(-1,1)';
+	} else if (e.key === 'ArrowRight' || e.key === 'Right') {
+		moveHorizontal(avatar, 50);
+		avatar.style.transform = 'scale(1,1)';
+	}
+	if (isTouching(avatar, coin)) moveCoin(); // if avatar touch coin call moveCoin
+});
+
+const moveVertical = (element, amount) => {
+	const currTop = extractPos(element.style.top);
+	element.style.top = `${currTop + amount}px`;
+};
+const moveHorizontal = (element, amount) => {
+	const currLeft = extractPos(element.style.left);
+	element.style.left = `${currLeft + amount}px`;
+};
+
+const extractPos = (pos) => {
+	if (!pos) return 100;
+	return parseInt(pos.slice(0, -2)); // removes 'px' from pos string and turn it into number
+};
+
+const moveCoin = () => {
+	const x = Math.floor(Math.random() * window.innerWidth);
+	const y = Math.floor(Math.random() * window.innerHeight);
+	coin.style.top = `${y}px`;
+	coin.style.left = `${x}px`;
+};
+
+moveCoin();
