7bf6cd19090994b5b0d00b593bf0dbeda4982ab6
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-05-30 23:35:01 +0500

    return promise

---
+++ 37_return_promise.js
@@ -0,0 +1,19 @@
+const makeDogPromise = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const rand = Math.random();
+			if (rand < 0.5) {
+				resolve();
+			} else {
+				reject();
+			}
+		}, 5000);
+	});
+};
+makeDogPromise()
+	.then(() => {
+		console.log('YAY WE GOT A DOG!!!!');
+	})
+	.catch(() => {
+		console.log(':( NO DOG');
+	});
