a2f691081734da996943af62caaff3f083f9a161
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-03 05:50:55 +0500

    async/await error handling

--- 48_await_intro.js
+++ 48_await_intro.js
@@ -14,3 +14,28 @@ getPlanets();
 // getPlanets().then((res) => {
 // 	console.log(res.data);
 // });
+
+// ========== ERROR HANDLING ==========
+
+// ONE OPTION FOR ERROR HANDLING...
+async function getPlanets() {
+	//Invalid URL...
+	const res = await axios.get('https://swapi.co/api/planeklsajdalksts/');
+	console.log(res.data);
+}
+
+getPlanets().catch((err) => {
+	console.log('IN CATCH!!!');
+	console.log(err);
+});
+
+// ANOTHER OPTION...
+async function getPlanets() {
+	try {
+		const res = await axios.get('https://swapi.co/api/planeklsajdalksts/');
+		console.log(res.data);
+	} catch (e) {
+		console.log('IN CATCH!', e);
+	}
+}
+getPlanets();
