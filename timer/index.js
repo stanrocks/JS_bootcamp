66b279601c0b95ba22cec8f410eb2747f51b2fae
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-06 01:29:26 +0500

    timer - first pass of animation

--- timer/index.js
+++ timer/index.js
@@ -3,13 +3,23 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
+// get circle view
+const circle = document.querySelector('circle');
+// calculate circle perimeter length
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+// set stroke-dasharray equals to perimeter
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
+
 // run new Timer instance
 const timer = new Timer(durationInput, startButton, pauseButton, {
 	onStart() {
 		console.log('Timer started');
 	},
 	onTick() {
-		console.log('Timer just ticked down');
+		circle.setAttribute('stroke-dashoffset', currentOffset);
+		currentOffset = currentOffset - 50;
 	},
 	onComplete() {
 		console.log('Timer is completed');
