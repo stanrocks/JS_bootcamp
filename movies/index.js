2c347971fabf46d7c1ef459fc9ab17ec91a45423
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-06 05:36:08 +0500

    movie fight - delay search while user typing

--- movies/index.js
+++ movies/index.js
@@ -9,6 +9,19 @@ const fetchData = async (searchTerm) => {
 };
 
 const input = document.querySelector('input');
-input.addEventListener('input', (event) => {
-	fetchData(event.target.value);
-});
+
+let timeoutId;
+
+// filtering user input. wait while user typing, then fetch
+const onInput = (event) => {
+	// after second or next inputs - reset timer
+	if (timeoutId) {
+		clearTimeout(timeoutId);
+	}
+	// if timer goes out - fetch data
+	timeoutId = setTimeout(() => {
+		fetchData(event.target.value);
+	}, 1000);
+};
+
+input.addEventListener('input', onInput);
