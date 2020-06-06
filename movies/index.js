3c840d6c78acfd266422ac1e35619205ac0799ec
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-06 21:46:24 +0500

    movie fight - move HTML generation to JS

--- movies/index.js
+++ movies/index.js
@@ -14,7 +14,20 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 // get data from API
 // async mode since gonna use promise
