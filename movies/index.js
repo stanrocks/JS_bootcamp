a497c8b9f94c04c77e2a7361c2d687395e5c2c72
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-06 05:02:28 +0500

    movie fight - fetched some data

--- movies/index.js
+++ movies/index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'c4a8ca95',
+			s: 'avengers'
+		}
+	});
+	console.log(response.data);
+};
+
+fetchData();
