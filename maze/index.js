e48908064fd9a6f60a98327dbad7194d05c5f084
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-08 02:10:04 +0500

    maze - boilerplate

--- maze/index.js
+++ maze/index.js
@@ -24,10 +24,16 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-// create shape
+// create shape (200,200 - position of rectangle center, 50,50 - size)
 const shape = Bodies.rectangle(200, 200, 50, 50, {
+	// rectangle is static and is not affected by gravity
 	isStatic: true
 });
 
 // add created shape to the world
 World.add(world, shape);
+
+// check what the world is
+console.log(world);
+// shows array objects that exist in world
+console.log(world.bodies);
