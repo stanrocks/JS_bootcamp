75ad786c05f35057ef58c238863a4145ee2f010d
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-08 02:31:33 +0500

    maze - drawing borders

--- maze/index.js
+++ maze/index.js
@@ -24,16 +24,27 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-// create shape (200,200 - position of rectangle center, 50,50 - size)
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-	// rectangle is static and is not affected by gravity
-	isStatic: true
-});
-
-// add created shape to the world
-World.add(world, shape);
+// // create shape (200,200 - position of rectangle center, 50,50 - size)
+// const shape = Bodies.rectangle(200, 200, 50, 50, {
+// 	// rectangle is static and is not affected by gravity
+// 	isStatic: true
+// });
+// // add created shape to the world
+// World.add(world, shape);
 
 // check what the world is
-console.log(world);
+// console.log(world);
 // shows array objects that exist in world
-console.log(world.bodies);
+// console.log(world.bodies);
+
+// Walls
+const walls = [
+	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+	Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+	Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+	Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
+
+World.add(world, walls);
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
