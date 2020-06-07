57bac1b95a416af271c6ed075c919a1d0aec411d
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-07 12:13:31 +0500

    maze - first try with matter

--- maze/index.js
+++ maze/index.js
@@ -1,1 +1,33 @@
-console.log('HI!!!');
+// create matter object consist of matter elements
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+// create new engine
+const engine = Engine.create();
+
+// access to world created with engine
+const { world } = engine;
+
+// show content on screen
+const render = Render.create({
+	// render inside document.body
+	element: document.body,
+	// specify what engine to use
+	engine: engine,
+	// define options - window size
+	options: {
+		width: 800,
+		height: 600
+	}
+});
+
+// execute render
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+// create shape
+const shape = Bodies.rectangle(200, 200, 50, 50, {
+	isStatic: true
+});
+
+// add created shape to the world
+World.add(world, shape);
