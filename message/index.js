b6601fe48a6989affc1c1f35bbd32b004205e967
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-08 13:24:19 +0500

    secret message - finished

--- message/index.js
+++ message/index.js
@@ -1,7 +1,27 @@
+// destruct hash
+const { hash } = window.location;
+
+// remove # from hash
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#message-show').classList.remove('hide');
+
+	document.querySelector('h1').innerHTML = message;
+}
+
 document.querySelector('form').addEventListener('submit', (event) => {
 	event.preventDefault();
 
-	const input = document.querySelector('input');
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#link-form').classList.remove('hide');
 
-	console.log(input.value);
+	const input = document.querySelector('#message-input');
+	// console.log(input.value);
+	const encrypted = btoa(input.value);
+
+	const linkInput = document.querySelector('#link-input');
+	linkInput.value = `${window.location}#${encrypted}`;
+	linkInput.select();
 });
