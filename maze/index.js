06b2ecc416e8a2e1abfeadd7451f4632c1367643
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-08 08:09:58 +0500

    maze - generate paths

--- maze/index.js
+++ maze/index.js
@@ -2,7 +2,7 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
 // maze generation config
-const cells = 5; // for vertical and horizontal
+const cells = 3; // for vertical and horizontal
 const width = 600;
 const height = 600;
 
@@ -47,7 +47,21 @@ World.add(world, walls);
 
 // 2. Maze generation
 
-// 2.1 Grid generation - square itself - to store 'have been here before'
+// Shuffle utility function - randomized path generation. Take array and reorder elements randomly
+const shuffle = (arr) => {
+	let counter = arr.length;
+	// swap each element at least 1 time
+	while (counter > 0) {
+		const index = Math.floor(Math.random() * counter);
+		counter--;
+		const temp = arr[counter];
+		arr[counter] = arr[index];
+		arr[index] = temp;
+	}
+	return arr;
+};
+
+// 2.1 Grid generation - cell itself - to store 'have been visited'
 // create 2d array (3 * 3)
 
 // Create array of 3 'null' values (doesn't really matter what is there),
@@ -56,7 +70,7 @@ World.add(world, walls);
 const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
 // console.log(grid);
 
-// 2.2 Grid borders generation - square edges
+// 2.2 Grid borders generation - cell edges
 // Verticals have 3 rows, 2 columns
 // Horizontals have 2 rows, 3 columns
 
@@ -67,3 +81,78 @@ const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
 const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
 const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
 // console.log(verticals, horizontals);
+
+// 2.3 Generate starting position
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+// console.log(startRow, startColumn);
+
+// 2.4. Generate paths
+const stepThroughCell = (row, column) => {
+	// If I have visited the cell at [row, column], then return
+	if (grid[row][column]) {
+		return;
+	}
+	// Mark this cell as being visited
+	grid[row][column] = true;
+	// Assemble randomly-ordered list of neighbors (top, right, bottom, left)
+	const neighbors = shuffle([
+		[
+			row - 1,
+			column,
+			'up'
+		],
+		[
+			row,
+			column + 1,
+			'right'
+		],
+		[
+			row + 1,
+			column,
+			'down'
+		],
+		[
+			row,
+			column - 1,
+			'left'
+		]
+	]);
+	// console.log(neighbors);
+	// For each neighbor...
+	for (let neighbor of neighbors) {
+		// deconstruct nextRow and nextColumn from neighbor
+		const [
+			nextRow,
+			nextColumn,
+			direction
+		] = neighbor;
+		// See if that neighbor is out of bounds
+		if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+			// move on to next neighbor
+			continue;
+		}
+		// If we have visited that neighbor, continue to next neighbor
+		if (grid[nextRow][nextColumn]) {
+			// move on to next neighbor
+			continue;
+		}
+		// Remove wall from either horizontals or verticals
+		if (direction === 'left') {
+			verticals[row][column - 1] = true;
+		} else if (direction === 'right') {
+			verticals[row][column] = true;
+		} else if (direction === 'up') {
+			horizontals[row - 1][column] = true;
+		} else if (direction === 'down') {
+			horizontals[row][column] = true;
+		}
+
+		// Visit that next cell
+		stepThroughCell(nextRow, nextColumn);
+	}
+};
+
+// Initiate from starting position
+stepThroughCell(startRow, startColumn);
+// console.log(grid);
