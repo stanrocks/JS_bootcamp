b7c9832298c787e476c8040405a4d7578feed305
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-11 04:52:10 +0500

    node list app - ordered list refactored

--- node/list/index.js
+++ node/list/index.js
@@ -5,14 +5,45 @@ fs.readdir(process.cwd(), (err, filenames) => {
 		console.log(err);
 	}
 
-	// BAD CODE HERE!!!!!!!!!!
+	// Different output for files and folders
+	// // BAD CODE HERE!!!!!!!!!!
+	// // bad because order of filenames is not consistent
+	// for (let filename of filenames) {
+	// 	fs.lstat(filename, (err, stats) => {
+	// 		if (err) {
+	// 			console.log(err);
+	// 		}
+	// 		console.log(filename, stats.isFile());
+	// 	});
+	// }
+	// // BAD CODE COMPLETE
+
+	// Let's make a solution with proper order of names
+	// Option 1 - using array (not greatest solution)
+	// get array full of nulls
+	const allStats = Array(filenames.length).fill(null);
+
 	for (let filename of filenames) {
+		const index = filenames.indexOf(filename);
+
 		fs.lstat(filename, (err, stats) => {
 			if (err) {
 				console.log(err);
 			}
-			console.log(filename, stats.isFile());
+
+			allStats[index] = stats;
+
+			// check every item in array
+			const ready = allStats.every((stats) => {
+				return stats; // if stat is null, false will be returned
+			});
+
+			// if array is filled, print all items in order
+			if (ready) {
+				allStats.forEach((stats, index) => {
+					console.log(filenames[index], stats.isFile());
+				});
+			}
 		});
 	}
-	// BAD CODE COMPLETE
 });
