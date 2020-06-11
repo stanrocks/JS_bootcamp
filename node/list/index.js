8dcd41ca694ef4e386bf1cace11c46b05fa71b4e
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-11 05:19:09 +0500

    node list app - ordered list refactor - using promises

--- node/list/index.js
+++ node/list/index.js
@@ -1,49 +1,91 @@
 const fs = require('fs');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+// use this for 'BAD CODE' below and for Option 1 'using array'
+// fs.readdir(process.cwd(), (err, filenames) => {
+// 	if (err) {
+// 		console.log(err);
+// 	}
+
+// Different output for files and folders
+// // BAD CODE HERE!!!!!!!!!!
+// // bad because order of filenames is not consistent
+// for (let filename of filenames) {
+// 	fs.lstat(filename, (err, stats) => {
+// 		if (err) {
+// 			console.log(err);
+// 		}
+// 		console.log(filename, stats.isFile());
+// 	});
+// }
+// // BAD CODE COMPLETE
+
+// Let's make a solution with proper order of names
+// Option 1 - using array (not greatest solution)
+// get array full of nulls
+// const allStats = Array(filenames.length).fill(null);
+
+// for (let filename of filenames) {
+// 	const index = filenames.indexOf(filename);
+
+// 	fs.lstat(filename, (err, stats) => {
+// 		if (err) {
+// 			console.log(err);
+// 		}
+
+// 		allStats[index] = stats;
+
+// 		// check every item in array
+// 		const ready = allStats.every((stats) => {
+// 			return stats; // if stat is null, false will be returned
+// 		});
+
+// 		// if array is filled, print all items in order
+// 		if (ready) {
+// 			allStats.forEach((stats, index) => {
+// 				console.log(filenames[index], stats.isFile());
+// 			});
+// 		}
+// 	});
+// }
+// });
+
+// Option 2 - using promises
+// serial execution (one operation at time, so it is very slow, not async)
+fs.readdir(process.cwd(), async (err, filenames) => {
 	if (err) {
 		console.log(err);
 	}
 
-	// Different output for files and folders
-	// // BAD CODE HERE!!!!!!!!!!
-	// // bad because order of filenames is not consistent
-	// for (let filename of filenames) {
-	// 	fs.lstat(filename, (err, stats) => {
-	// 		if (err) {
-	// 			console.log(err);
-	// 		}
-	// 		console.log(filename, stats.isFile());
-	// 	});
-	// }
-	// // BAD CODE COMPLETE
-
-	// Let's make a solution with proper order of names
-	// Option 1 - using array (not greatest solution)
-	// get array full of nulls
-	const allStats = Array(filenames.length).fill(null);
-
 	for (let filename of filenames) {
-		const index = filenames.indexOf(filename);
-
-		fs.lstat(filename, (err, stats) => {
-			if (err) {
-				console.log(err);
-			}
-
-			allStats[index] = stats;
-
-			// check every item in array
-			const ready = allStats.every((stats) => {
-				return stats; // if stat is null, false will be returned
-			});
-
-			// if array is filled, print all items in order
-			if (ready) {
-				allStats.forEach((stats, index) => {
-					console.log(filenames[index], stats.isFile());
-				});
-			}
-		});
+		try {
+			const stats = await lstat(filename);
+			console.log(filename, stats.isFile());
+		} catch (err) {
+			console.log(err);
+		}
 	}
 });
+
+// Option 2 - using promises (not greatest solution)
+// serial execution (one operation at time, so it is very slow, not async)
+// Promise Method 1
+// const lstat = (filename) => {
+// 	return new Promise((resolve, reject) => {
+// 		fs.lstat(filename, (err, stats) => {
+// 			if (err) {
+// 				reject(err);
+// 			}
+// 			resolve(stats);
+// 		});
+// 	});
+// };
+
+// Promise Method 2
+// const util = require('util'); // https://nodejs.org/docs/latest/api/util.html
+// const lstat = util.promisify(fs.lstat);
+
+// Promise Method 3
+// const lstat = fs.promises.lstat; // https://nodejs.org/docs/latest/api/fs.html#fs_fspromises_lstat_path_options
+
+// Promise Method 3.1 - destructuring
+const { lstat } = fs.promises; // https://nodejs.org/docs/latest/api/fs.html#fs_fspromises_lstat_path_options
