943c756da89258fb29f5b445b6d911f6cec0c97d
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-11 23:56:01 +0500

    runner - app plan

--- node/watchit/index.js
+++ node/watchit/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
 console.log('I was executed');
+
+// nodemon-clone app
+// Issues:
+// 1. Need to detect when a file changes -> use a package called 'chokidar' to detect file changes
+// 2. Provide some help to users of our CLI tool -> use a package called 'caporal' to build our CLI tool
+// 3. Need to figure out how to execute some JS code from within a JS program -> use standard library module 'child_process' to execute a program
+
+// https://www.npmjs.com/package/chokidar
+// https://www.npmjs.com/package/caporal
