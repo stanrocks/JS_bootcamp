a052c9d1a3d7b48e9ce0da0731af20167347faaf
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-13 18:13:04 +0500

    ecomm express - parsing user input

--- ecomm/index.js
+++ ecomm/index.js
@@ -17,6 +17,23 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
+	// get access to email, password, password confirmation in buffer-form
+	// buffer example: <Buffer 65 6d 61 69 6c 3d 73 64 66 26 70 61 73 73 77 6f 72 64 3d 73 64 66 26 70 61 73 73 77 6f 72 64 43>
+	//
+	req.on('data', (data) => {
+		// convert buffer to text and split to array elements
+		// string example: email=sdf&password=sdf&passwordConfirmation=sdf
+		const parsed = data.toString('utf8').split('&');
+		const formData = {};
+		for (let pair of parsed) {
+			const [
+				key,
+				value
+			] = pair.split('=');
+			formData[key] = value;
+		}
+		console.log(formData);
+	});
 	res.send('Account created!');
 });
 
