93942171b6b1dbbb22ce2087ab917fa7eaba327a
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-13 14:54:30 +0500

    ecomm express init

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+const express = require('express');
+
+const app = express();
+
+// routes
+// root route
+app.get('/', (req, res) => {
+	res.send('hi there');
+});
+
+app.listen(3000, () => {
+	console.log('Listening');
+});
