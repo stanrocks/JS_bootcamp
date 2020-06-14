3abea3636070219d2164b1433f44a16371c0d9c1
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-14 17:37:31 +0500

    ecomm - create method logic

--- ecomm/repositories/users.js
+++ ecomm/repositories/users.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 class UsersRepository {
+	// create repo with empty array
 	constructor(filename) {
 		if (!filename) {
 			throw new Error('Creating a repository requires a filename');
@@ -21,11 +22,21 @@ class UsersRepository {
 			fs.writeFileSync(filename, '[]');
 		}
 	}
+
 	async getAll() {
 		// Open 'filename'-file using fsPromises.readFile - https://nodejs.org/docs/latest/api/fs.html#fs_fspromises_readfile_path_options
 		// Parse contents
 		// Return parsed data
 		return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
+	}
+
+	async create(attrs) {
+		// {email: '23ewrwe@dkfj.co, passwords: 'kjlfajf' - plaintext oops, will fix later}
+		// read data at first, then add new object
+		const records = await this.getAll();
+		records.push(attrs);
+		// write updated 'records' array back to this.filename
+		await fs.promises.writeFile(this.filename, JSON.stringify(records));
 	}
 }
 
@@ -37,6 +48,8 @@ class UsersRepository {
 // 2. test getAll
 const test = async () => {
 	const repo = new UsersRepository('users.json');
+
+	await repo.create({ email: 'test@test.com', passwords: 'passwords' });
 
 	const users = await repo.getAll();
 	console.log(users);
