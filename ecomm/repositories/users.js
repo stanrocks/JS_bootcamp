70dc4f14d4366af0be9344540a6c97b903a30263
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-14 16:47:20 +0500

    ecomm - implement users repository

---
+++ ecomm/repositories/users.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+class UsersRepository {
+	constructor(filename) {
+		if (!filename) {
+			throw new Error('Creating a repository requires a filename');
+		}
+		// storing passed filename in instance variable
+		this.filename = filename;
+
+		// https://nodejs.org/docs/latest/api/fs.html
+		// There are 3 versions:
+		// 1. fs.access (using callback)
+		// 2. fs.accessSync (sync version - pauses execution - bad for performance)
+		// it is better to use option 2, cause we need file to exist at first for all other code to work. and we call that only one time for entire lifecycle of our app.
+		// 3. fsPromises.access - access based on promise
+		try {
+			fs.accessSync(this.filename);
+		} catch (err) {
+			// if file not exist - create new one
+			fs.writeFileSync(filename, '[]');
+		}
+	}
+}
+
+const repo = new UsersRepository('users.json');
+
+// Tests
+
+// 1. should throw error:
+// new UsersRepository();
