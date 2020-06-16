0d258559a507d44cc58c1eec178cabb6cc0c3e2b
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-16 05:01:03 +0500

    ecomm - product routes

--- ecomm/index.js
+++ ecomm/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(
 
 // routers
 app.use(authRouter);
+app.use(productsRouter);
 
 // routes
 
