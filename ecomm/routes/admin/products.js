2348e1bdf22aab67a84b41e043906410fa71a2d9
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-18 01:49:37 +0500

    ecomm - product - image upload - accessing uploaded file

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {});
 router.get('/admin/products/new', (req, res) => {
@@ -16,14 +19,12 @@ router.post(
 		requireTitle,
 		requirePrice
 	],
+	upload.single('image'),
 	(req, res) => {
 		const errors = validationResult(req);
 		// console.log(errors);
-		console.log(req.body);
+		console.log(req.file); // shows multer parsing result of user data uploaded through form
 
-		req.on('data', (data) => {
-			console.log(data.toString());
-		});
 		res.send('submitted');
 	}
 );
