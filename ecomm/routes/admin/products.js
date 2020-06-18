f437d1fc87323d69c00529b26c98ae9c689528f9
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-19 01:51:21 +0500

    ecomm - edit products - displaying edit form

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -5,7 +5,10 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
+const products = require('../../repositories/products');
+const { response } = require('express');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -43,5 +46,18 @@ router.post(
 		res.redirect('/admin/products');
 	}
 );
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+	// console.log(req.params.id);
+	const product = await productsRepo.getOne(req.params.id);
+
+	if (!product) {
+		return res.send('Product not found');
+	}
+
+	res.send(productsEditTemplate({ product }));
+});
+
+router.post('/admin/products/:id/edit', requireAuth, async (req, res) => {});
 
 module.exports = router;
