99107e167006fa0e6683f39a76755e22adb146bf
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-21 04:30:44 +0500

    testing - displaying autocomplete

--- testing/movies/test/autocomplete.test.js
+++ testing/movies/test/autocomplete.test.js
@@ -1,1 +1,12 @@
-it('works!!!', () => {});
+it('Shows an autocomplete', () => {
+	createAutoComplete({
+		root: document.querySelector('#target'),
+		// fake response from API
+		fetchData() {
+			return [ { Title: 'Avengers' }, { Title: 'Not Avengers' }, { Title: 'Some other movie' } ];
+		},
+		renderOption(movie) {
+			return movie.Title;
+		}
+	});
+});
