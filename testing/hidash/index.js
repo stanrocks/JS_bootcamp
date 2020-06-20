24a12b07f7d783a27572bdcb19e84a0217041747
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-20 16:12:08 +0500

    testing - map

--- testing/hidash/index.js
+++ testing/hidash/index.js
@@ -10,5 +10,15 @@ module.exports = {
 		for (let index in arr) {
 			fn(arr[index], index);
 		}
+	},
+
+	map(arr, fn) {
+		const result = [];
+
+		for (let i = 1; i < arr.length; i++) {
+			result.push(fn(arr[i], i));
+		}
+
+		return result;
 	}
 };
--- testing/hidash/index.test.js
+++ testing/hidash/index.test.js
@@ -1,5 +1,6 @@
-const { forEach } = require('./index');
+const { forEach, map } = require('./index');
 
+// test forEach
 let sum = 0;
 forEach([ 1, 2, 3 ], (value) => {
 	sum += value;
@@ -7,4 +8,20 @@ forEach([ 1, 2, 3 ], (value) => {
 
 if (sum !== 6) {
 	throw new Error('Expected summing array to equal 6');
+}
+
+// test map
+const result = map([ 1, 2, 3 ], (value) => {
+	return value * 2;
+});
+// result === [2,4,6]
+
+if (result[0] !== 2) {
+	throw new Error(`Expected to find 2, but bound ${result[0]}`);
+}
+if (result[1] !== 4) {
+	throw new Error(`Expected to find 4, but bound ${result[1]}`);
+}
+if (result[2] !== 6) {
+	throw new Error(`Expected to find 2, but bound ${result[2]}`);
 }
