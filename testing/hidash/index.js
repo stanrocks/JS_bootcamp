9142494def1cbfb7588791ebe73094180bc3c1b9
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-20 14:28:54 +0500

    testing - testing implementation

--- testing/hidash/index.js
+++ testing/hidash/index.js
@@ -1,9 +1,14 @@
 // forEach function
 module.exports = {
 	forEach(arr, fn) {
-		for (let i = 0; i < arr.length; i++) {
-			const value = arr[i];
-			fn(value, i);
+		// for (let i = 0; i < arr.length; i++) {
+		// 	const value = arr[i];
+		// 	fn(value, i);
+		// }
+
+		// refactor
+		for (let index in arr) {
+			fn(arr[index], index);
 		}
 	}
 };
