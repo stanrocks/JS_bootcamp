0f525e0583e3f9c8976d8be8bda1197460b3a7b4
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-22 04:50:41 +0500

    test me - collecting test files

--- testing/tme/runner.js
+++ testing/tme/runner.js
@@ -1,15 +1,29 @@
-const fs = require('fs');
+const fs = require('fs'); // https://nodejs.org/docs/latest/api/fs.html
+const path = require('path'); // https://nodejs.org/docs/latest/api/path.html
 
 // collects files to be tested
 class Runner {
 	constructor() {
-		this.files = [];
+		this.testFiles = [];
 	}
 
 	async collectFiles(targetPath) {
 		const files = await fs.promises.readdir(targetPath);
 
-		return files;
+		for (let file of files) {
+			const filepath = path.join(targetPath, file);
+			const stats = await fs.promises.lstat(filepath);
+			// https://nodejs.org/docs/latest/api/fs.html#fs_stats_isdirectory
+
+			// look for a file with name '*.test.js*'
+			if (stats.isFile() && file.includes('.test.js')) {
+				this.testFiles.push({ name: filepath });
+			} else if (stats.isDirectory()) {
+				const childFiles = await fs.promises.readdir(filepath);
+
+				files.push(...childFiles.map((f) => path.join(file, f)));
+			}
+		}
 	}
 }
 
