c46a88dfef8f36cae1e51320aadd7affaf6b9d6f
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-06-21 07:00:43 +0500

    testing - test dropdown opens after search

--- testing/movies/test/autocomplete.test.js
+++ testing/movies/test/autocomplete.test.js
@@ -1,4 +1,31 @@
-it('Shows an autocomplete', () => {
+// helper function
+// helps solve issue when tested function is executed after some time (i.e. debounce)
+const waitFor = (selector) => {
+	return new Promise((resolve, reject) => {
+		// check periodically if function is resolved
+		const interval = setInterval(() => {
+			if (document.querySelector(selector)) {
+				// stop interval and timeout timer
+				clearInterval(interval);
+				clearTimeout(timeout);
+				resolve();
+			}
+		}, 30);
+
+		// wait for long time, if function is not resolved - means it's been rejected
+		const timeout = setTimeout(() => {
+			// stop interval running if time is out
+			clearInterval(interval);
+			reject();
+		}, 2000);
+	});
+};
+
+// code that runs before each 'it'
+beforeEach(() => {
+	// reset dropdown area
+	document.querySelector('#target').innerHTML = '';
+
 	createAutoComplete({
 		root: document.querySelector('#target'),
 		// fake response from API
@@ -9,10 +36,25 @@ it('Shows an autocomplete', () => {
 			return movie.Title;
 		}
 	});
+});
 
-	// check if dropdown is not active (expected behavior)
+// check if dropdown is not active (expected behavior)
+it('Dropdown starts closed', () => {
 	const dropdown = document.querySelector('.dropdown');
-
 	// chai-expect manual: https://www.chaijs.com/guide/styles/#expect
 	expect(dropdown.className).not.to.include('is-active');
+});
+
+it('After searching. dropdown opens up', async () => {
+	const input = document.querySelector('input');
+	// put text to text input
+	input.value = 'avengers';
+	// fake DOM event as if user typed text into text input
+	input.dispatchEvent(new Event('input'));
+
+	await waitFor('.dropdown-item');
+
+	const dropdown = document.querySelector('.dropdown');
+	// dropdown should be active
+	expect(dropdown.className).to.include('is-active');
 });
